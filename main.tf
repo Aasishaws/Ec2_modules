9690ecd73c2e3e2178ce2fc570f886bd23c3c7c0
Aasishaws <115416682+Aasishaws@users.noreply.github.com>
2022-10-16 19:06:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,23 +1,12 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-}
-
-provider "aws" {
-  shared_credentials_file = "~/.aws/credentials"
-  region = var.region
-}
-[root@ip-172-31-17-204 terraform]# cat main.tf
 module "master_module-1" {
   source = ".//module-1"
   masterec2_instance_type = var.master_ec2instance
+  count = var.master_count
+  master_name = var.master_tag
 }
 module "slave_module-1" {
   source = ".//module-2"
   slaveec2_instance_type = var.slave_ec2instance
+  count = var.slave_count
+  slave_name = var.slave_tag
 }
-
