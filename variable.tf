496ec669b79473d5c058afc183d75c370865d5d6
Aasishaws <115416682+Aasishaws@users.noreply.github.com>
2022-10-16 19:08:01 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,14 @@
+variable "region" {
+}
+ variable "master_ec2instance" {
+}
+ variable "slave_ec2instance" {
+}
+ variable "master_tag" {
+}
+ variable "slave_tag" {
+}
+ variable "master_count" {
+}
+ variable "slave_count" {
+}
